0ef95c9679ac5efa357f05a6adc9dba2f941b6de
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2023-11-16 15:34:05 -0300

    testing: without google credentials

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify
-from google.cloud import firestore
-from google.oauth2.service_account import Credentials
-import os
+# from google.cloud import firestore
+# from google.oauth2.service_account import Credentials
+# import os
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("service_firebase.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 app = Flask(__name__)
 
 # carregando as credenciais do JSON
-credentials = Credentials.from_service_account_file(os.environ.get
-                                                    ('GOOGLE_APPLICATION_CREDENTIALS'))
-db = firestore.Client(credentials=credentials)
+# credentials = Credentials.from_service_account_file(os.environ.get
+#                                                     ('GOOGLE_APPLICATION_CREDENTIALS'))
+# db = firestore.Client(credentials=credentials)
 # inicializando o firestore com a credencial
 
 # Rota para obter dados de usuários
