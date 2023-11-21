99397987c9786bf472e55a119f2e0bb83ffac069
Rebeca Primo <rebecarodriguesprimo036@gmail.com>
2023-11-21 17:23:08 -0300

    apis Delete criadas

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify
-# from google.cloud import firestore
-# from google.oauth2.service_account import Credentials
-# import os
 
 import firebase_admin
 from firebase_admin import credentials
